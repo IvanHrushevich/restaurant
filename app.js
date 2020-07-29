ab6ca2b09ec7000bf5007eb56e70932d8682968f
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-07-29 16:29:40 +0300

    connect to mongoDB

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/auth");
 const analyticsRoutes = require("./routes/analytics");
 const categoryRoutes = require("./routes/category");
 const orderRoutes = require("./routes/order");
 const positionRoutes = require("./routes/position");
+const keys = require("./config/keys");
 
 const app = express();
+
+mongoose
+  .connect(keys.MONGO_URI)
+  .then(() => console.log("mongoDB has been connected"))
+  .catch((err) => console.log(error));
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://ivan:QCeOquLH0nKtiDvt@restaurant.b0gzf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
