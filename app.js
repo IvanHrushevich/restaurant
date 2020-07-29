bfae03c4a69924e1d9c7dc43f219989d0602b6f1
Ivan Hrushevich <ivan_hrushevich@epam.com>
2020-07-29 13:48:49 +0300

    add some packages, create models

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const authRoutes = require('./routes/auth');
-const analyticsRoutes = require('./routes/analytics');
-const categoryRoutes = require('./routes/category');
-const orderRoutes = require('./routes/order');
-const positionRoutes = require('./routes/position');
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const orderRoutes = require("./routes/order");
+const positionRoutes = require("./routes/position");
 
 const app = express();
 
-app.use('/api/auth', authRoutes);
-app.use('/api/analyticsRoutes', analyticsRoutes);
-app.use('/api/categoryRoutes', categoryRoutes);
-app.use('/api/orderRoutes', orderRoutes);
-app.use('/api/positionRoutes', positionRoutes);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/analyticsRoutes", analyticsRoutes);
+app.use("/api/categoryRoutes", categoryRoutes);
+app.use("/api/orderRoutes", orderRoutes);
+app.use("/api/positionRoutes", positionRoutes);
 
 module.exports = app;
