108d7fc6082ac717ef9d5a669b52764de2961204
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-07-29 23:50:54 +0300

    update auth controller

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGO_URI:
-    "mongodb+srv://ivan:QCeOquLH0nKtiDvt@restaurant.b0gzf.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "mongodb+srv://ivan:QCeOquLH0nKtiDvt@restaurant.b0gzf.mongodb.net/ivan?retryWrites=true&w=majority",
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,19 @@
+const User = require("../models/User");
+
 module.exports.login = (req, res) => {
   res.status(200).json({
     login: true,
   });
 };
 
-module.exports.register = (req, res) => {
-  res.status(200).json({
-    register: true,
-  });
+module.exports.register = async (req, res) => {
+  const userInDb = await User.findOne({ email: req.body.email });
+
+  if (userInDb) {
+    res
+      .status(400)
+      .json({ message: "This email already exists. Try another one." });
+  } else {
+    //
+  }
 };
