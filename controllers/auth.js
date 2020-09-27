6539b21d9ba18fd4917607db46489b197e0e26b2
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-09-27 18:35:31 +0300

    feat: implement auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,39 @@
-const User = require("../models/User");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-module.exports.login = (req, res) => {
-  res.status(200).json({
-    login: true,
-  });
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+module.exports.login = async (req, res) => {
+  const userInDb = await User.findOne({ email: req.body.email });
+
+  if (userInDb) {
+    const passwordResult = bcrypt.compareSync(
+      req.body.password,
+      userInDb.password
+    );
+
+    if (passwordResult) {
+      const token = jwt.sign(
+        {
+          email: userInDb.email,
+          userId: userInDb._id,
+        },
+        keys.JWT,
+        { expiresIn: 60 * 60 }
+      );
+
+      res.status(200).json({ token: `Bearer ${token}` });
+    } else {
+      res.status(401).json({
+        message: 'Passwords do not match. Try again.',
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: 'User was not found',
+    });
+  }
 };
 
 module.exports.register = async (req, res) => {
@@ -12,8 +42,20 @@ module.exports.register = async (req, res) => {
   if (userInDb) {
     res
       .status(400)
-      .json({ message: "This email already exists. Try another one." });
+      .json({ message: 'This email already exists. Try another one.' });
   } else {
-    //
+    const salt = bcrypt.genSaltSync(10);
+
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, salt),
+    });
+
+    try {
+      await user.save();
+      res.status(201).json(user);
+    } catch (err) {
+      //
+    }
   }
 };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
@@ -14,4 +14,4 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("users", userSchema);
+module.exports = mongoose.model('users', userSchema);
