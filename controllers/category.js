c7365c3565261c4f94e4eaedfe9ed2e7c6a09d6f
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-12-05 22:32:19 +0300

    partially implement Category controller

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,46 @@
-module.exports.getAll = (req, res) => {
-  res.json({ message: 'Categories' });
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getAll = async (req, res) => {
+  try {
+    const categories = await Category.find({ user: req.user.id });
+    res.status(200).json(categories);
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
 
-module.exports.getById = (req, res) => {
-  //
+module.exports.getById = async (req, res) => {
+  try {
+    const category = await Category.find({ user: req.params.id });
+    res.status(200).json(category);
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
 
-module.exports.remove = (req, res) => {
-  //
+module.exports.remove = async (req, res) => {
+  try {
+    await Category.remove({ _id: req.params.id });
+    await Position.remove({ category: req.params.id });
+
+    res.status(200).json({ message: 'Category has been removed' });
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
 
-module.exports.create = (req, res) => {
-  //
+module.exports.create = async (req, res) => {
+  try {
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
 
-module.exports.update = (req, res) => {
-  //
+module.exports.update = async (req, res) => {
+  try {
+  } catch (err) {
+    errorHandler(res, err);
+  }
 };
