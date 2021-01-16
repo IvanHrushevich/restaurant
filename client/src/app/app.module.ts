74b76a98a4a8fe63eb81658224899776683e7766
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-01-16 09:35:09 +0300

    temp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { OverviewPageComponent } from './overview-page/overview-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { TokenInterceptor } from './shared';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
-import { OverviewPageComponent } from './overview-page/overview-page.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +24,10 @@ import { OverviewPageComponent } from './overview-page/overview-page.component';
     SiteLayoutComponent,
     RegisterPageComponent,
     OverviewPageComponent,
+    AnalyticsPageComponent,
+    HistoryPageComponent,
+    OrderPageComponent,
+    CategoriesPageComponent,
   ],
   imports: [
     BrowserModule,
